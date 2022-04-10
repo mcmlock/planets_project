2c0678b13c4c5c1c904f705a71c6c762fa726635
Miles Morlock <milesmorlockcode@gmail.com>
2022-04-10 19:05:53 -0400

    Data from kepler_data.csv is converted to Js object

---
+++ index.cjs
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const parser = require('csv-parse');
+
+const parse = parser.parse;
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+         comment: '#',
+         columns: true
+    }))
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(`An error has occured: ${err}`);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
