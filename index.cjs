e99692f27f992895e6ffbed21879f3f68d7feabf
Miles Morlock <milesmorlockcode@gmail.com>
2022-04-11 15:47:08 -0400

    Added a function that narrows done the habitable planets from the data

--- index.cjs
+++ index.cjs
@@ -2,7 +2,24 @@ const fs = require('fs');
 const parser = require('csv-parse');
 
 const parse = parser.parse;
-const results = [];
+const habitablePlanets = [];
+
+const isHabitable = planet => {
+    let habitable = true;
+    // Makes sure the planet exists
+    if (planet['koi_disposition'] !== 'CONFIRMED') {
+        habitable = false;
+    }
+    // Makes sure it gets the right amount of light
+    if (planet['koi_insol'] < .36 || planet['koi_insol'] > 1.11) {
+        habitable = false;
+    }
+    // Makes sure it's the right size
+    if (planet['koi_prad'] > 1.6) {
+        habitable = false;
+    }
+    return habitable;
+}
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
@@ -10,12 +27,14 @@ fs.createReadStream('kepler_data.csv')
          columns: true
     }))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitable(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.log(`An error has occured: ${err}`);
     })
     .on('end', () => {
-        console.log(results);
+        console.log(`${habitablePlanets.length} possibly habitable planets were found`);
         console.log('done');
     });
